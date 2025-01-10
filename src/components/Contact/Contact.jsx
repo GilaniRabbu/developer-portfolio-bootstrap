8c5e47ae9ad071b9069fab13ffb7458263a12a68
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2025-01-10 23:03:59 +0600

    updated contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -30,34 +30,32 @@ const Contact = () => {
   };
 
   return (
-    <section className="py-5 contact-section">
+    <section className="contact-section">
       <div className="container">
         <h2 className="section-title mb-5 text-center font-bonny-bold">
           Contact Me.
         </h2>
         <div className="mt-5 d-flex gap-5 gap-md-5 flex-column flex-md-row">
           <div className="contact-info">
-            <h2 className="w-75 mb-4 font-bonny-regular">
+            <h2 className="mb-4 font-bonny-regular contact-info-title">
               I am always here to assist you and respond to your inquiries
             </h2>
-            <p className="mb-5 contact-desc">
+            <p className="mb-4 contact-desc">
               Whether you have questions, need assistance, or simply want to
               share your thoughts, I am eager to hear from you. Feel free to
               connect with me through any of the methods below, and I will get
               back to you promptly. I look forward to hearing from you!
             </p>
-            <div className="d-flex flex-wrap justify-content-between gap-2 mb-5">
-              <div className="contact-info-item">
-                <h3>Location</h3>
-                <p>Dhaka, South Khilgaon</p>
-              </div>
-              <div className="contact-info-item">
-                <h3>E-mail</h3>
-                <p>toufiqjilanirabbu7@gmail.com</p>
-              </div>
+            <div className="contact-info-item mb-4">
+              <h3>Location</h3>
+              <p>Dhaka, South Khilgaon</p>
+            </div>
+            <div className="contact-info-item mb-4">
+              <h3>E-mail</h3>
+              <p>toufiqjilanirabbu7@gmail.com</p>
             </div>
             <div>
-              <h3 className="mb-2 social-network-title">Social Network</h3>
+              <h3 className="social-network-title">Social Network</h3>
               <div className="d-flex gap-2 social-network">
                 <a
                   href="https://www.facebook.com/GilaniRabbu"
